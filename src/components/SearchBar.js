ea7109e9bf8cdcc97fb570cecb67608da814f2d5
Bilal Koçak <kocakbilal04@gmail.com>
2019-05-02 02:13:54 +0300

    acilis sayfasinin kabasi yapildi
    Logo tamam,
    Inputlar duzenlenmeli

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    render() {
+        return(
+            <div>
+                Burası bar
+            </div>
+        )
+    }
+}
+
+export default SearchBar
