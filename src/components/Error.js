e8c3d1f4a40ce0fdd043831b78bc94f23931fc88
Bilal Koçak <kocakbilal04@gmail.com>
2019-05-02 13:02:45 +0300

    404 saygası eklendi
    daha düzenlenmedi

---
+++ src/components/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Error extends React.Component{
+    render() {
+        return(
+            <div>
+                404
+            </div>
+        )
+    }
+}
+
+export default Error
