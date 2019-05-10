c103d3fc393d3f8155f812abf9bd2abc49f17cfa
Bilal Koçak <kocakbilal04@gmail.com>
2019-05-10 04:44:32 +0300

    Statless component haline getirildi

--- src/components/404/Error.js
+++ src/components/404/Error.js
@@ -1,35 +1,25 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import './Error.css'
-class Error extends React.Component {
 
-    render() {
-
-        return (
-
-            <div>
-                <div className="App">
-                    <div id="notfound">
-                        <div className="notfound">
-                            <div className="notfound-404">
-                                <h1>4<span></span>4</h1>
-                            </div>
-                            <h2>Oops! Page Not Be Found</h2>
-                            <p>Sorry but the page you are looking for does not exist, have been removed. name changed or
-                                is temporarily unavailable</p>
-                            <Link to={"/"}>
-                                <div className="searchButton backButton">BACK TO HOME</div>
-                            </Link>
-                        </div>
+const Error = () =>
+    <div>
+        <div className="App">
+            <div id="notfound">
+                <div className="notfound">
+                    <div className="notfound-404">
+                        <h1>4<span></span>4</h1>
                     </div>
-
+                    <h2>Oops! Page Not Be Found</h2>
+                    <p>Sorry but the page you are looking for does not exist, have been removed. name changed or
+                        is temporarily unavailable</p>
+                    <Link to={"/"}>
+                        <div className="searchButton backButton">BACK TO HOME</div>
+                    </Link>
                 </div>
             </div>
 
-        )
-    }
-
-}
-
+        </div>
+    </div>
 
 export default Error;
--- src/components/NoResult/NoResult.js
+++ src/components/NoResult/NoResult.js
@@ -1,31 +1,20 @@
 import React from 'react';
 import './NoResult.css'
 
-class NoResult extends React.Component {
 
-
-    render() {
-
-        return (
-
-            <div>
-                <div id="noResult">
-                    <div className="noResult">
-                        <div className="noResultOops">
-                            <h1>Oops!</h1>
-                        </div>
-                        <h2>404 - Page not found</h2>
-                        <p>The page you are looking for might have been removed had its name changed or is
-                            temporarily unavailable.</p>
-
-                    </div>
+const NoResult = () =>
+    <div>
+        <div id="noResult">
+            <div className="noResult">
+                <div className="noResultOops">
+                    <h1>Oops!</h1>
                 </div>
+                <h2>404 - Page not found</h2>
+                <p>The page you are looking for might have been removed had its name changed or is
+                    temporarily unavailable.</p>
+
             </div>
-
-        )
-    }
-
-}
-
+        </div>
+    </div>
 
 export default NoResult;
